e56fee137a7aca1a9998a08be364f8f5a547f621
Geovane Fiirst <geovanefiirst@hotmail.com>
2026-08-17 09:58:42 -0400

    feat: adicionando busca de ip local para mascara /24

--- Services/ScannerService.cs
+++ Services/ScannerService.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 
 namespace dokkaebi_os.Services;
@@ -9,7 +11,7 @@ public class ScannerService
 {
     public async Task<List<Device>> GetDevicesAsync()
     {
-        const string baseIp = "10.0.0.";
+        var baseIp = GetBaseIp();
 
         var tasks = Enumerable
             .Range(2, 253)
@@ -45,5 +47,33 @@ public class ScannerService
             .Where(device => device is not null)
             .Cast<Device>()
             .ToList();
+    }
+
+    private string GetBaseIp()
+    {
+        foreach (var network in NetworkInterface.GetAllNetworkInterfaces())
+        {
+            if (network.OperationalStatus != OperationalStatus.Up)
+                continue;
+            
+            if (network.NetworkInterfaceType != NetworkInterfaceType.Ethernet &&
+                network.NetworkInterfaceType != NetworkInterfaceType.Wireless80211)
+            {
+                continue;
+            }
+
+            foreach (var address in network.GetIPProperties().UnicastAddresses)
+            {
+                if (address.Address.AddressFamily != AddressFamily.InterNetwork)
+                    continue;
+
+                var ip = address.Address.ToString();
+                var parts = ip.Split('.');
+
+                return $"{parts[0]}.{parts[1]}.{parts[2]}.";
+            }
+        }
+
+        throw new Exception("Nenhum endereço IPv4 encontrado.");
     }
 }
